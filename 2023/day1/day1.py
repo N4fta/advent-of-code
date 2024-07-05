4a357d9e90c6a3cf83ace811c0ae164a03011e48
Neo <540432@fhict.nl>
2024-07-05 12:59:13 +0200

    day1 completed

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -1,5 +1,16 @@
-import os
 
+fs = open ("2023\day1\input.txt")
 
+totalSum = 0
 
-print(text)
+line: str
+for line in iter(fs.readline, ''):
+    first, last = '', ''
+    for char in line:
+        if char.isnumeric ():
+            if first == "": first = char
+            last = char
+
+    #print(first, last)
+    totalSum += int(first+last)
+print(totalSum)
